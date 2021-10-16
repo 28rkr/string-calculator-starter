c3c0395d9803739b1f25d2be4efbb4aeb9280d7a
Rohit Kumar Gupta <25rohitgupta@gmail.com>
2021-10-16 23:32:07 +0530

    testing add method for 2 numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,6 +6,10 @@ public class StringCalculator {
     	if(input.equals("")) {
     		return 0;
     	}
+    	else if(input.contains(",")) {
+    		String s[] = input.split(",");
+    		return Integer.parseInt(s[0]) + Integer.parseInt(s[1]); 		
+    	}
     	else {
     		return Integer.parseInt(input);
     	} 
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -17,4 +17,13 @@ public class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+    public void string_with_two_numbers_should_return_number_as_int() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1,2"));
+        assertEquals(233, stringCalculator.add("11,222"));
+        assertEquals(176, stringCalculator.add("151,25"));
+        assertEquals(2100, stringCalculator.add("100,2000"));
+    }
 }
